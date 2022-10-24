90f9cb3108ff2559ee5cc63817e71c984cd5a10a
Josh <54758130+JoshuaMoni@users.noreply.github.com>
2022-10-24 22:15:59 +1300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,33 @@
+var modal = document.getElementById("myModal");
+var incomplete = document.getElementById("incompleteModal");
+
+function displayModal(){ 
+  modal.style.display = "block";
+}
+
+function notImplementented(){ 
+  incomplete.style.display = "block";
+}
+
+// Get the button that opens the modal
+var btn = document.getElementById("trigger-modal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }else if (event.target == incomplete){ 
+    incomplete.style.display = "none";
+  }
+}
